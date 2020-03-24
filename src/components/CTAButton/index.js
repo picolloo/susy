099cd4240a003356406cb6fc6feaeef1459396dd
picolloo <lucaspicolloo@gmail.com>
2020-03-24 15:51:59 -0300

    CTAButton implemented

---
+++ src/components/CTAButton/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import styled from "styled-components"
+
+import colors from "../../styles/colors"
+
+const Button = styled.a`
+  background: ${colors.primary};
+  border: 1px solid ${colors.primary};
+  border-radius: 4px;
+  padding: 10px 20px;
+  outline: none;
+  max-width: 205px;
+  color: #fff;
+  text-decoration: none;
+  text-align: center;
+
+  letter-spacing: 1px;
+  font-size: 16px;
+
+  &:visited {
+    color: #fff;
+  }
+
+  &:hover {
+    background: none;
+
+    color: ${colors.primary} !important;
+  }
+`
+
+export default function CTAButton({ children }) {
+  return <Button style={{ color: "#fff", fontWeight: 600 }}>{children}</Button>
+}
